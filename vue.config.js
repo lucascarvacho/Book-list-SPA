d303042423fb4f5fcad0acaced4933fd8cac0496
Luks <164937302+lucascarvacho@users.noreply.github.com>
2026-09-06 13:54:04 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/nombre-de-tu-repositorio/'
+    ? '/Book-list-SPA/'
     : '/'
 })
